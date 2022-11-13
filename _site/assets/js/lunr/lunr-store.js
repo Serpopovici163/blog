0089f2c0da6b0572a2613be224dfceab1e6e4bce
Serban Popovici <serpopovici163@gmail.com>
2022-11-12 19:56:12 -0500

    commit before rocketry email

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,20 +1,14 @@
 var store = [{
-        "title": "IP Camera Hacking Toolkit",
-        "excerpt":"Exploit collection/automation for networked cameras This is a project I haphazardly began upon discovering FIND CVE # which allows an attacker to bypass authentication on Hikvision cameras using a crafted URL. The exploit is rather simplistic but made me consider how useful camera exploits are when infiltrating facilities with unknown...","categories": [],
+        "title": "DIY 3D Printed Powered Ascender",
+        "excerpt":"Shouldn’t work but it might I figured that, considering people have 3D printed functional guns, surely it must be possible to 3D print an ascender without worrying about the material’s strength. Initial designs are solely for prototyping and, as such, have no accomodations for the ESCs, batteries, or any hardware...","categories": [],
         "tags": [],
-        "url": "/camera-toolkit/",
+        "url": "/powered-ascender/",
         "teaser": null
       },{
-        "title": "Annota Application",
-        "excerpt":"Annota is my first ever attempt at developing software for Android devices. As part of a second year course, we were challenged to develop an application that could digitize a user’s notes in order to categorize and index them. This application is far from exemplar for a variety of reasons...","categories": [],
+        "title": "Mining Rig Temperature Control",
+        "excerpt":"A solution for outdoor mining rigs during winter I briefly decided to invest in a few graphics cards with all the recent hype surrounding cryptocurrencies and mining. I figured, with GPU prices today, it would be rather impossible to lose money from such an investment and I wanted to try...","categories": [],
         "tags": [],
-        "url": "/annota-mobile-application/",
-        "teaser": null
-      },{
-        "title": "Two G-class Stage Rocket w/ Basic Telemetry",
-        "excerpt":"Experimenting with rocketry electronics While I doubt I will ever get a high power rocketry license, I do thoroughly enjoy developing and flying model rockets. None of my past rockets have incorporated any amount of electronics so the purpose of this project is to get a feel for rocketry electronics...","categories": [],
-        "tags": [],
-        "url": "/two-stage-rocket/",
+        "url": "/mining-rig-temp-control/",
         "teaser": null
       },{
         "title": "Jaguar (Medium-sized VTOL UAV Platform)",
@@ -23,10 +17,10 @@ var store = [{
         "url": "/jaguar/",
         "teaser": null
       },{
-        "title": "LTE Mavic Pro",
-        "excerpt":"Cellular-based drone control has been a longstanding goal with the purpose of creating UAVs requiring minimal user input to function. I have found the need for constant input to significantly reduce the useability of drones for recon during my adventures because neither myself nor the people with me can dedicate...","categories": [],
+        "title": "Roof Anchor Drone",
+        "excerpt":"A physical infiltration tool This device was originally designed in hopes of rooftopping the second tallest building in Canada at the time of this writing: The St. Regis hotel in Toronto. The concern was that the lower roof at the St. Regis hotel is accessible while the higher roof is...","categories": [],
         "tags": [],
-        "url": "/lte-mavic-pro/",
+        "url": "/roof-anchor-drone/",
         "teaser": null
       },{
         "title": "Smart Holographic Sight",
@@ -41,15 +35,27 @@ var store = [{
         "url": "/camera-privacy-hack/",
         "teaser": null
       },{
+        "title": "LTE Mavic Pro",
+        "excerpt":"Cellular-based drone control has been a longstanding goal with the purpose of creating UAVs requiring minimal user input to function. I have found the need for constant input to significantly reduce the useability of drones for recon during my adventures because neither myself nor the people with me can dedicate...","categories": [],
+        "tags": [],
+        "url": "/lte-mavic-pro/",
+        "teaser": null
+      },{
         "title": "Modernizing my 2013 Subaru BRZ",
         "excerpt":"I recently acquired a car for recreational purposes since I had saved up a decent amount of money and didn’t feel like investing it. I purchased a BRZ because they are built in conjunction with Toyota and this should theoretically imply that they are relatively reliable so I won’t need...","categories": [],
         "tags": [],
         "url": "/brz/",
         "teaser": null
       },{
-        "title": "Roof Anchor Drone",
-        "excerpt":"A physical infiltration tool This device was originally designed in hopes of rooftopping the second tallest building in Canada at the time of this writing: The St. Regis hotel in Toronto. The concern was that the lower roof at the St. Regis hotel is accessible while the higher roof is...","categories": [],
+        "title": "LoRa Sensor Suite",
+        "excerpt":"A scalable dynamic solution for monitoring activity in unknown environments At some point in 2021, a friend and I had just finished exploring a construction site and as we were preparing to leave on the first floor, a security guard haphazardly walked in. kLuckily we were out of sight and...","categories": [],
         "tags": [],
-        "url": "/roof-anchor-drone/",
+        "url": "/lora-sensor-suite/",
+        "teaser": null
+      },{
+        "title": "Two G-class Stage Rocket w/ Basic Telemetry",
+        "excerpt":"Experimenting with rocketry electronics While I doubt I will ever get a high power rocketry license, I do thoroughly enjoy developing and flying model rockets. None of my past rockets have incorporated any amount of electronics so the purpose of this project is to get a feel for rocketry electronics...","categories": [],
+        "tags": [],
+        "url": "/two-stage-rocket/",
         "teaser": null
       }]
